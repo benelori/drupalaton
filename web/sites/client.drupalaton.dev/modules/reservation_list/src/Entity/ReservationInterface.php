6151301b819bc3d1891e500a7aba1b1eb6807856
Bene Lorant <bene.lori89@gmail.com>
2017-08-12 07:44:31 +0200

    Created reservation entity.

---
+++ web/sites/client.drupalaton.dev/modules/reservation_list/src/Entity/ReservationInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\reservation_list\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\user\EntityOwnerInterface;
+
+/**
+ * Provides an interface for defining Reservation entities.
+ *
+ * @ingroup reservation_list
+ */
+interface ReservationInterface extends  ContentEntityInterface {
+
+  /**
+   * Gets the Reservation creation timestamp.
+   *
+   * @return int
+   *   Creation timestamp of the Reservation.
+   */
+  public function getCreatedTime();
+
+  /**
+   * Sets the Reservation creation timestamp.
+   *
+   * @param int $timestamp
+   *   The Reservation creation timestamp.
+   *
+   * @return \Drupal\reservation_list\Entity\ReservationInterface
+   *   The called Reservation entity.
+   */
+  public function setCreatedTime($timestamp);
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPnr();
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPnr($pnr);
+
+
+}
